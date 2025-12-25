81fa7cabc449cb1d2205cb185d757aa1da6b7284
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-25 12:11:28 +1100

    Add donut and textures
    Added a donut, and textures to the shapes as well

--- main.js
+++ main.js
@@ -8,16 +8,22 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const texture = new THREE.TextureLoader().load('cat.jpg')
+const material = new THREE.MeshBasicMaterial( { map: texture } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+const donut_geo = new THREE.TorusGeometry(10, 3, 16, 100);
+const donut_texture = new THREE.TextureLoader().load('donut.webp')
+const donut_tex = new THREE.MeshBasicMaterial({ map: donut_texture });
+const donut = new THREE.Mesh(donut_geo, donut_tex);
+scene.add(donut);
+
+camera.position.z = 18;
 
 function animate() {
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    donut.rotation.x += 0.01;
+    donut.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 
